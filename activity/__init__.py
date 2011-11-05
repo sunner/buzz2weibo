6e41adea907b08b6daeb0604dfb39435c49da145
Sun Zhigang <sunner@gmail.com>
2011-11-05 16:30:39 +0800

    删除buzz的各种activity。再见了buzz

--- activity/__init__.py
+++ activity/__init__.py
@@ -14,14 +14,4 @@ __version__ = '1.0'
 __author__ = 'Sun Zhigang'
 __license__ = 'GPL'
 
-from buzz import BuzzActivity
-from greader import GoogleReaderActivity
-from wildcard import WildcardActivity
-from mobile import MobileActivity
-from gtalk import GooglechatstatusActivity
-from twitter import TwitterActivity
-from gmaps import GoogleMapsformobileActivity
-from sunner import HelloWorldActivity
-from checkin import GoogleLatitudeCheckinActivity
-from flickr import FlickrActivity
 from gplus import GooglePlusActivity
